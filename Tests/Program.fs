8fbfbea380339c9263d84d86a317d0dc79aac4eb
Viktor Karasev <viktorkarasev27@gmail.com>
2022-09-23 02:48:55 +0300

    Implemented RSM for calculator language

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -4,7 +4,6 @@ open CFPQ_GLL
 open CFPQ_GLL.Common
 open CFPQ_GLL.GLL
 open CFPQ_GLL.InputGraph
-open CFPQ_GLL.RSM
 open Tests.InputGraph
 open CFPQ_GLL.SPPF
 open Expecto
@@ -12,35 +11,33 @@ open Expecto
 
 let config = {FsCheckConfig.defaultConfig with maxTest = 10000}
 
-  
+
 let go() =
     let graph = InputGraph([|TerminalEdge(0<inputGraphVertex>, 0<terminalSymbol>, 0<inputGraphVertex>)
-                             TerminalEdge(0<inputGraphVertex>, 1<terminalSymbol>, 0<inputGraphVertex>)                             
+                             TerminalEdge(0<inputGraphVertex>, 1<terminalSymbol>, 0<inputGraphVertex>)
                            |])
     let startV = [|0<inputGraphVertex>|]
     let q = Tests.GLLTests.simpleLoopRSMForDyckLanguage ()
-    
+
     let startVertices,_ = graph.ToCfpqCoreGraph (HashSet startV)
     let result = GLL.eval startVertices q GLL.AllPaths
 
     match result with
-    | QueryResult.MatchedRanges ranges -> 
+    | QueryResult.MatchedRanges ranges ->
       let sppf = q.OriginalStartState.NonTerminalNodes.ToArray()
       let actual = TriplesStoredSPPF (sppf, Dictionary())
-      
+
       actual.ToDot "1.dot"
-      
+
       Tests.GLLTests.dumpResultToConsole actual
-    | _ -> failwith "Unexpected result." 
+    | _ -> failwith "Unexpected result."
     0
- 
+
 [<EntryPoint>]
 let main argv =
-    Tests.runTestsWithCLIArgs [] [||] (testList "debug tests" [Tests.GLLTests.``Form V#``])
-   
+    Tests.runTestsWithCLIArgs [] [||] Tests.RSMCalculator.``Calculator RSM tests on linear input``
+    //Tests.runTestsWithCLIArgs [] [||] (testList "debug tests" [Tests.GLLTests.``Form V#``])
     //Tests.runTestsWithCLIArgs [] [||] (testList "all tests" [Tests.GLLTests.tests])
-    
     //Tests.runTestsWithCLIArgs [] [||] (testList "all tests" [Tests.DistancesTests.tests])
     //go ()
-        
-    
+
